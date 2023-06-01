ffba8e6e41ed9857366d7943ed5b56697bbd26aa
Ivanka Nagornyuk <IvNagornyuk@gmail.com>
2023-06-01 06:44:46 +0300

    up

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -18,3 +18,70 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// import React, { useState } from "react";
+// import {
+//   StyleSheet,
+//   View,
+//   TextInput,
+//    TouchableWithoutFeedback, // імпорт компонента обгортки
+//   Keyboard, // імпорт компонента клавіатури
+//   // KeyboardAvoidingView,
+//   Platform,
+// } from "react-native";
+// import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+// export default function App() {
+//   const [name, setName] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const nameHandler = (text) => setName(text);
+//   const passwordHandler = (text) => setPassword(text);
+
+//   const onLogin = () => {
+//     Alert.alert("Credentials", `${name} + ${password}`);
+//   };
+
+//   return (
+//     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+//       <KeyboardAwareScrollView behavior={Platform.OS == "ios" ? "padding" : "height"}>
+//       <View style={styles.container}>
+//         {/* <KeyboardAvoidingView
+//           behavior={Platform.OS == "ios" ? "padding" : "height"}
+//         > */}
+//           <TextInput
+//             value={name}
+//             onChangeText={nameHandler}
+//             placeholder="Username"
+//             style={styles.input}
+//           />
+//           <TextInput
+//             value={password}
+//             onChangeText={passwordHandler}
+//             placeholder="Password"
+//             secureTextEntry={true}
+//             style={styles.input}
+//           />
+//           <Button title={"Login"} style={styles.input} onPress={onLogin} />
+//         {/* </KeyboardAvoidingView> */}
+//       </View></KeyboardAwareScrollView>
+//     </TouchableWithoutFeedback>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     backgroundColor: "#ecf0f1",
+//   },
+//   input: {
+//     width: 200,
+//     height: 44,
+//     padding: 10,
+//     borderWidth: 1,
+//     borderColor: "black",
+//     marginBottom: 10,
+//   },
+// });
